597a2150e90f28cf4a2317a1ee9a386bdb4b972c
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-15 15:16:48 +0530

    complete

--- src/main/java/com/security/securityTest/MyUserDetailsService.java
+++ src/main/java/com/security/securityTest/MyUserDetailsService.java
@@ -1,16 +1,26 @@
 package com.security.securityTest;
 
 import com.security.securityTest.models.MyUserDetails;
+import com.security.securityTest.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+        user.orElseThrow(()->new UsernameNotFoundException(username+" : user not found"));
+        return user.map(MyUserDetails::new).get();
 
-        return new MyUserDetails(username);
     }
 }
